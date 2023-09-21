7414cbb2132fe771e5301939cb40080091e3673b
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-09-21 15:12:12 +0200

    api ?

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Test, TestService } from './services/test-service/test.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+
+  testValue: Observable<Test> = this.service.getTest();
+
+  constructor(private service: TestService) {}
+}
