c1e01aaea60e5a2a557b13be1c9700a2436e5291
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-10-06 18:19:24 +0200

    magicForm

--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import {MatCardModule} from '@angular/material/card'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +18,12 @@ import { RegisterComponent } from './component/register/register.component';
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+	MatCardModule,
+	ReactiveFormsModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatButtonModule
   ]
 })
 export class PublicModule { }
