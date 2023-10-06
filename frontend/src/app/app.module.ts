cf0e46c54b90100519317f742445ab5ea12f4225
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-10-06 18:09:02 +0200

    snackabr

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSnackBarModule} from '@angular/material/snack-bar'; 
 
 @NgModule({
   declarations: [
@@ -11,7 +14,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-	HttpClientModule
+	HttpClientModule,
+ 	BrowserAnimationsModule,
+	MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
